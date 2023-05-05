e4fab0a7dba92dca87a4f13afd27e2d22455f5a6
timmy16744 <timhughes92@gmail.com>
2023-05-06 00:23:24 +0930

    initial

---
+++ firebaseConfig.js
@@ -0,0 +1,13 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCCOEHw6QQ7rpbYtiqqsZyxM1bQ_AweLMQ",
+  authDomain: "gr8numb3rstasklist.firebaseapp.com",
+  projectId: "gr8numb3rstasklist",
+  storageBucket: "gr8numb3rstasklist.appspot.com",
+  messagingSenderId: "755410376709",
+  appId: "1:755410376709:web:daf0fe9fd1bba80137a46c",
+  databaseURL: "http://127.0.0.1:9000/?ns=gr8numb3rstasklist"
+  //databaseURL: "https://gr8numb3rstasklist-default-rtdb.firebaseio.com",
+};
+
+module.exports = { firebaseConfig }; // Use module.exports syntax
---
+++ index.js
@@ -0,0 +1,68 @@
+import firebase from "firebase/app";
+import { getDatabase, ref, push, on } from "firebase/database";
+import { firebaseConfig } from "./firebaseConfig.js"; // Import firebaseConfig
+const firebase = require("firebase/app");
+require("firebase/database");
+const { firebaseConfig } = require("./firebaseConfig"); // Use require syntax
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database(); // Initialize the database
+
+const taskForm = document.getElementById("task-form");
+const tasksList = document.getElementById("tasks-list");
+
+function saveTask(task) {
+  const tasksRef = ref(db, "tasks");
+  push(tasksRef, task);
+}
+
+function fetchClients() {
+  const tasksRef = ref(db, "tasks");
+  on(tasksRef, (snapshot) => {
+    tasksList.innerHTML = "";
+    snapshot.forEach((childSnapshot) => {
+      const task = childSnapshot.val();
+      displayTask(task);
+    });
+  });
+}
+
+function displayTask(task) {
+  const taskItem = document.createElement("li");
+  taskItem.textContent = `${task.clientName} - ${task.loanAmount}`;
+  tasksList.appendChild(taskItem);
+}
+
+function resetForm() {
+  taskForm.reset();
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const clientNameInput = document.getElementById("client-name");
+  const loanAmountInput = document.getElementById("loan-amount");
+
+  const clientName = clientNameInput.value;
+  const loanAmount = loanAmountInput.value;
+
+  const task = {
+    clientName,
+    loanAmount,
+  };
+
+  saveTask(task);
+  resetForm();
+}
+
+// Connect to the emulated database instance
+if (location.hostname === "localhost") {
+  const database = getDatabase(firebase.app());
+  database.useEmulator("localhost", 9000);
+}
+
+taskForm.addEventListener("submit", handleSubmit);
+document.getElementById("fetch-data-button").addEventListener("click", fetchClients);
+
+fetchClients();
